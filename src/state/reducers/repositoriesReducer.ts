4d766353c0adaa346b1998a6ee237757564f33bf
surajkumar85 <49224173+surajkumar85@users.noreply.github.com>
2023-03-11 10:49:03 +0530

    added action type

--- src/state/reducers/repositoriesReducer.ts
+++ src/state/reducers/repositoriesReducer.ts
@@ -4,16 +4,40 @@ interface RepositoriesState {
   data: string[];
 }
 
+enum ActionType {
+  SEARCH_REPOISTORIES = "search_repoistories",
+  SEARCH_REPOISTORIES_ERROR = "search_repoistories_error",
+  SEARCH_REPOISTORIES_SUCCESS = "search_repoistories_success",
+}
+
+interface SearchRepoistoriesAction {
+  type: ActionType.SEARCH_REPOISTORIES;
+}
+
+interface SearchRepoistoriesErrorAction {
+  type: ActionType.SEARCH_REPOISTORIES_ERROR;
+  payload: string;
+}
+interface SearchRepoistoriesSuccessAction {
+  type: ActionType.SEARCH_REPOISTORIES_SUCCESS;
+  payload: string[];
+}
+
+type Action =
+  | SearchRepoistoriesAction
+  | SearchRepoistoriesErrorAction
+  | SearchRepoistoriesSuccessAction;
+
 export default function reducer(
   state: RepositoriesState,
-  action: any
+  action: Action
 ): RepositoriesState {
   switch (action.type) {
-    case "search_repoistories":
+    case ActionType.SEARCH_REPOISTORIES:
       return { loading: true, error: null, data: [] };
-    case "search_repoistories_success":
+    case ActionType.SEARCH_REPOISTORIES_SUCCESS:
       return { loading: false, error: null, data: action.payload };
-    case "search_repoistories_error":
+    case ActionType.SEARCH_REPOISTORIES_ERROR:
       return { loading: false, error: action.payload, data: [] };
     default:
       return state;
