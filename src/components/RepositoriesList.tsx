cc0f083cb482bb5092b3e039a30eee6bba4a101f
surajkumar85 <49224173+surajkumar85@users.noreply.github.com>
2023-03-11 12:37:07 +0530

    added repo

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,9 +1,13 @@
 import { useState } from "react";
 import { useActions } from "../hooks/useActions";
+import { useSelector } from "../hooks/useTypedSelector";
 
 const RepositoriesList: React.FC = () => {
   const [term, setTerm] = useState<string>("");
   const { searchRepositories } = useActions();
+  const { data, error, loading } = useSelector(
+    (state: any) => state.repositories
+  );
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -16,6 +20,11 @@ const RepositoriesList: React.FC = () => {
         <input value={term} onChange={(e) => setTerm(e.target.value)} />
         <button>Search</button>
       </form>
+      {error && <h3>{error}</h3>}
+      {loading && <h3>Loading....</h3>}
+      {!error &&
+        !loading &&
+        data.map((name: string) => <div key={name}>{name}</div>)}
     </div>
   );
 };
--- src/state/reducers/index.ts
+++ src/state/reducers/index.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import repositoriesReducer from "./repositoriesReducer";
 
-const reducer = combineReducers({
+const reducers = combineReducers({
   repositories: repositoriesReducer,
 });
 
-export default reducer;
+export default reducers;
+
+export type RootState = ReturnType<typeof reducers>;
